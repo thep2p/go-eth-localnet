afb65e6d828aa2e2b3c2e37ac12d65f353461001
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-05-31 21:27:06 -0500

    Adds Geth node launch test
    Adds a test case to verify the successful launch and closure of a single Geth node. This test ensures that the node can be started with a given configuration, and that its URL is correctly formatted. It also tests the correct closure of the node.

--- internal/node/geth_test.go
+++ internal/node/geth_test.go
@@ -1,1 +1,37 @@
-package node
+package node_test
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
+	"github.com/thep2p/go-eth-localnet/internal/node"
+	"github.com/thep2p/go-eth-localnet/internal/testutils"
+	"os"
+	"testing"
+)
+
+func TestSingleNodeLaunch(t *testing.T) {
+	logger := zerolog.New(os.Stdout).Level(zerolog.InfoLevel)
+	tmp := testutils.NewTempDir(t)
+	port := testutils.RandomPort(t)
+
+	cfg := node.Config{
+		ID:      0,
+		DataDir: tmp.Path(),
+		P2PPort: port,
+		RPCPort: port + 1000,
+	}
+
+	launcher := node.NewLauncher(logger)
+	handle, err := launcher.Launch(cfg)
+	require.NoError(t, err)
+	require.NotNil(t, handle)
+	require.Contains(t, handle.NodeURL(), "enode://")
+
+	defer func() {
+		err := handle.Close()
+		if err != nil {
+			logger.Fatal().Err(err).Msg("failed to close node")
+		}
+		logger.Info().Msg("Node closed successfully")
+	}()
+}
