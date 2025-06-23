b1b3e76d6bb4e196d8666d5e35b86e7afa6d6519
Y H, Ph.D. <19204398+thep2p@users.noreply.github.com>
2025-06-23 15:12:14 -0700

    Update internal/testutils/ethereum.go
    Co-authored-by: Sanaz Taheri <35961250+staheri14@users.noreply.github.com>

--- internal/testutils/ethereum.go
+++ internal/testutils/ethereum.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 )
 
+// PrivateKeyFixture generates a new random private key for use in tests.
+// It fails the test immediately if key generation does not succeed.
 func PrivateKeyFixture(t *testing.T) *ecdsa.PrivateKey {
 	priv, err := crypto.GenerateKey()
 	require.NoError(t, err, "failed to generate private key")
